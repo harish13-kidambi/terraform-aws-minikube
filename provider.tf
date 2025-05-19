50a01b992f233b1ff8363d33047be01ad3245a2e
harish13-kidambi <57281181+harish13-kidambi@users.noreply.github.com>
2025-05-19 12:25:14 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,21 +1,23 @@
 # which provider you are connecting to, version
 terraform {
+  required_version = ">= 1.3.0"
+
   required_providers {
     aws = {
-      source = "hashicorp/aws"
-      version = "5.4.0"
+      source  = "hashicorp/aws"
+      version = "~> 5.4.0"
     }
   }
 
   backend "s3" {
-        bucket = "terraform-k8-course"
-        key    = "minikube"
-        region = "ap-south-1"
-        dynamodb_table = "terraform-lock"
-    }
+    bucket         = "terraform-k8-course"
+    key            = "minikube/terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "terraform-lock"
+    encrypt        = true
+  }
 }
 
 provider "aws" {
-  # Configuration options
-  region = "ap-south-1"
+  region = var.aws_region
 }
