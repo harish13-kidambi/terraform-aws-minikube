14fdfd0b454996001cabe0500607baf028ba1caa
harish13-kidambi <57281181+harish13-kidambi@users.noreply.github.com>
2025-05-19 19:13:39 +0530

    Update minikube.tf

--- minikube.tf
+++ minikube.tf
@@ -13,9 +13,9 @@ module "minikube" {
   }
 
   addons = [
-    "https://raw.githubusercontent.com/kubernetes-sigs/metrics-server/master/components.yaml",
-    "https://raw.githubusercontent.com/kubernetes/dashboard/v2.7.0/aio/deploy/recommended.yaml",
-    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/storage-class.yaml",
-    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/external-dns.yaml"
+   "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/kubernetes-dashabord/init.sh",
+      "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/kubernetes-metrics-server/init.sh",
+      "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/external-dns/init.sh",
+      "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/kubernetes-nginx-ingress/init.sh"
   ]
 }
