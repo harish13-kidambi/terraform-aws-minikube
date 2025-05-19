d5ffb0e5a6f493743750c327da18cd5e8df3c296
harish13-kidambi <57281181+harish13-kidambi@users.noreply.github.com>
2025-05-19 13:38:59 +0530

    Update minikube.tf

--- minikube.tf
+++ minikube.tf
@@ -5,7 +5,7 @@ module "minikube" {
   cluster_name       = var.project_name
   aws_instance_type  = "t3.medium"
   ssh_public_key     = file(var.key_location)
-  aws_subnet_id      = var.public_subnet_cidr
+  aws_subnet_id      = var.public_subnet_ids
   hosted_zone = var.hosted_zone
   hosted_zone_private = false
   tags = {
