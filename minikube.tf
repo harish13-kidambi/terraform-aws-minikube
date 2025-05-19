2f36e498434ad6690f6bdca0fa4c01fa2e12d852
harish <harish.kidambi@gmail.com>
2025-05-19 18:45:50 +0530

    update all files

--- minikube.tf
+++ minikube.tf
@@ -1,11 +1,11 @@
 module "minikube" {
   source = "github.com/scholzj/terraform-aws-minikube"
 
-  aws_region         = var.aws_region
+  aws_region         = data.aws_region.current.name
   cluster_name       = var.project_name
   aws_instance_type  = "t3.medium"
-  ssh_public_key     = file(var.key_location)
-  aws_subnet_id      = var.public_subnet_ids[0]
+  ssh_public_key     = var.key_location
+  aws_subnet_id      = local.public_subnet_ids[0]
   hosted_zone = var.hosted_zone
   hosted_zone_private = false
   tags = {
